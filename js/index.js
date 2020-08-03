a2e2b2c9078ff2e422f0d115be27def5a50f120e
shela1216 <shela1216@gmail.com>
2020-08-03 16:26:02 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
         viewHeight: document.body.clientHeight,
         keyword:"",
         learncontent:"",
+        groupId:"",
         password:""
     }
     var vm = new Vue({
@@ -13,12 +14,33 @@
             PageHeight: function () {
                 return this.viewHeight - 147 + "px"
             },
+            args: function () {
+                var ret = {};
+                var str = this.params;
+                if (!str) {
+                    return ret;
+                }
+                var seg = str.replace(/^\?/, "").split('&');
+
+                for (var i = seg.length - 1; i >= 0; i--) {
+                    if (!seg[i]) {
+                        continue;
+                    }
+                    var s = seg[i].split('=');
+                    ret[s[0]] = s[1];
+                }
+
+                return ret;
+            },
         },
         mounted: function () {
             this.viewHeight = document.body.clientHeight;
             window.addEventListener('resize', this.heightChange);
         },
         created: function () {
+            if (this.args.groupId) {
+                this.groupId=this.args.groupId;
+            }
         },
 
         methods: {
